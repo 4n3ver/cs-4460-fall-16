3a6b8018d6307d3e8d67869738767c9c8cd7f3f7
Yoel Ivan <yivan3@gatech.edu>
2016-09-02 02:56:43 +0100

    move some styling to the style.css and add the transpiled src to the git

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+/**
+ * @author Yoel Ivan (yivan3@gatech.edu)
+ * @version 0.0a
+ */
+
 "use strict";
 
 import * as d3 from "d3";
@@ -37,78 +42,66 @@ const drawBarGraph = (bardata, labelx, labely) => {
             return "#424242";
         }
     };
-    const xLabelScale = d3.scaleBand()
-                          .domain(bardata.map(d => d[labelx]))
-                          .range([0, width])
-                          .padding(.2);
-    const yLabelScale = d3.scaleLinear()
-                          .domain([0, d3.max(bardata, d => d[labely])])
-                          .range([height, 0]);
     const xScale = d3.scaleBand()
-                     .domain(d3.range(0, bardata.length))
+                     .domain(bardata.map(d => d[labelx]))
                      .range([0, width])
                      .padding(.2);
     const yScale = d3.scaleLinear()
                      .domain([0, d3.max(bardata, d => d[labely])])
-                     .range([0, height]);
-    const xAxis = d3.axisBottom().scale(xLabelScale).ticks(bardata.length);
-    const yAxis = d3.axisLeft().scale(yLabelScale).ticks(10);
+                     .range([height, 0]);
 
-    let bars = d3.select('#chart').append('svg')
-                 .style("border-radius", "10px")
-                 .style("background", "#cfcfcf")
-                 .attr("width", width + margin.left + margin.right)
-                 .attr("height", height + margin.top + margin.bottom)
-                 .append("g")
-                 .attr("class", "bars")
-                 .attr("transform",
-                       `translate(${margin.left}, ${margin.top})`)
-                 .selectAll("rect").data(bardata)
-                 .enter().append("g");
+    let svg = d3.select('#chart').append('svg')
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom);
+    let bars = svg.append("g")
+                  .attr("class", "bars")
+                  .attr("transform",
+                        `translate(${margin.left}, ${margin.top})`)
+                  .selectAll("rect").data(bardata).enter()
+                  .append("g");
     let chart = bars.append("rect")
                     .style("fill", d => colorScale(d[labely]))
                     .attr("width", xScale.bandwidth())
-                    .attr("x", (d, i) => xScale(i))
+                    .attr("x", d => xScale(d[labelx]))
                     .attr("height", () => 0)
                     .attr("y", () => height)
-                    .style("outline", "1px solid #424242")
-                    .on("mouseenter", function (data) {
-                        d3.select(this).style("opacity", .5);
-                    }).on("mouseleave",
-                          function (data) {
-                              d3.select(this).style("opacity", 1);
-                          });
+                    .on("mouseenter",
+                        function (data) {
+                            d3.select(this).style("opacity", .5);
+                        })
+                    .on("mouseleave",
+                        function (data) {
+                            d3.select(this).style("opacity", 1);
+                        });
     let bartext = bars.append("text")
                       .text(d => d[labely])
                       .attr("text-anchor", "middle")
                       .attr("fill", d => colorTextScale(d[labely]))
-                      .attr("x", (d, i) => xScale(i) + xScale.bandwidth() / 2)
+                      .attr("x", d => xScale(d[labelx]) + xScale.bandwidth() / 2)
                       .attr("y", height)
                       .style("opacity", 0);
 
     chart.transition()
          .delay((d, i) => i * 20)
          .duration(2000)
-         .attr("height", d => yScale(d[labely]))
-         .attr("y", d => height - yScale(d[labely]))
+         .attr("height", d => height - yScale(d[labely]))
+         .attr("y", d => yScale(d[labely]))
          .ease(d3.easeElastic);
-
     bartext.transition()
            .delay((d, i) => i * 20)
            .duration(2000)
-           .attr("y", d => height - yScale(d[labely]) + 15)
+           .attr("y", d => yScale(d[labely]) + 15)
            .style("opacity", 1)
            .ease(d3.easeElastic);
 
-    let yGuide = d3.select("section#chart svg").append("g")
-                   .attr("transform",
-                         `translate(${margin.left}, ${margin.top})`);
-    yAxis(yGuide);
-
-    let xGuide = d3.select("section#chart svg").append("g")
-                   .attr("transform",
-                         `translate(${margin.left}, ${height + margin.top})`);
-    xAxis(xGuide);
+    svg.append("g")
+       .attr("transform",
+             `translate(${margin.left}, ${margin.top})`)
+       .call(d3.axisLeft(yScale));
+    svg.append("g")
+       .attr("transform",
+             `translate(${margin.left}, ${height + margin.top})`)
+       .call(d3.axisBottom(xScale));
 };
 
 d3.csv("data/State-GPA.csv")
